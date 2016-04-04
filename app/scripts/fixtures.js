1bbe4a96d5623739221c364999a1daabd41c55b8
Cameron James <cameron.w.james@live.com>
2016-04-04 15:20:49 -0700

    add controller to landing and collection templates

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,47 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: 161.71, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/blue' },
+        { title: 'Green', duration: 103.96, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/green' },
+        { title: 'Red', duration: 268.45, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/red' },
+        { title: 'Pink', duration: 153.14, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/pink'},
+        { title: 'Magenta', duration: 374.22, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/magenta'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumM83 = {
+    title: 'Hurry Up, We\'re Dreaming',
+    artist: 'M83',
+    label: 'Naive',
+    year: '2011',
+    albumArtURL: 'http://ecx.images-amazon.com/images/I/51ksz96s7TL.jpg',
+    songs: [
+        {title: 'Intro', duration: '5:22'},
+        {title: 'Midnight City', duration: '4:03'},
+        {title: 'Reunion', duration: '3:55'},
+        {title: 'Where the Boats Go', duration: '1:46'},
+        {title: 'Wait', duration: '5:43'},
+        {title: 'Raconte-Moi une Histoire', duration: '4:04'},
+        {title: 'Train to Pluton', duration: '1:15'},
+        {title: 'Claudia Lewis', duration: '4:31'}
+    ]
+};
