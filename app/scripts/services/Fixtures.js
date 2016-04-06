634061e1a77521c3f9b2ef7c6491bfb4cd5bda5d
Cameron James <cameron.w.james@live.com>
2016-04-06 01:21:59 -0700

    checkpoint 7 add play/pause functionality with Song Player service

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -9,11 +9,11 @@
             year: '1881',
             albumArtUrl: '/assets/images/album_covers/01.png',
             songs: [
-                { title: 'Blue', duration: 161.71, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/blue' },
-                { title: 'Green', duration: 103.96, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/green' },
-                { title: 'Red', duration: 268.45, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/red' },
-                { title: 'Pink', duration: 153.14, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/pink'},
-                { title: 'Magenta', duration: 374.22, audioUrl: 'C:/Users/Cameron/Desktop/bloc/bloc-jams-backup/assets/music/magenta'}
+                { title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue' },
+                { title: 'Green', duration: 103.96, audioUrl: '/assets/music/green' },
+                { title: 'Red', duration: 268.45, audioUrl: '/assets/music/red' },
+                { title: 'Pink', duration: 153.14, audioUrl: '/assets/music/pink'},
+                { title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta'}
             ]
         };
 
