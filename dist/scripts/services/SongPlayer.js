929de346ffd43bfb02902d4bd77555785f21970a
Cameron James <cameron.w.james@live.com>
2016-04-09 17:14:46 -0700

    bug fix: correct mistake I made from checkpoint 7

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -81,7 +81,7 @@
                 playSong(song);
             } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
