9a9b84435c5748114f376f0ae043a5db533f50e2
Cameron James <cameron.w.james@live.com>
2016-04-11 14:46:34 -0700

    assignment-11, refactor timecode filter with buzz.toTimer()

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,23 +1,10 @@
 (function() {
     function timecode() {
         return function(seconds) {
-            var seconds = Number.parseFloat(seconds);
-            
-            if(Number.isNaN(seconds)) {
-                return '-:--';
-            }
-            
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
-
-            var output = minutes + ':';
-
-            if (remainingSeconds < 10) {
-            output += '0';   
-            }
-
-            output += remainingSeconds;
+            output = buzz.toTimer(seconds);
+            if (output[0] === '0') {
+                return output.slice(1);
+            };
             
             return output;
         }
